996978a997f6197c71aa131c596708a5e21ec0be
hishamkodvavi <hkodvavi3@gatech.edu>
2018-09-05 13:10:37 -0400

    Hisham adding to antlabmain and fixing Person5

--- Person5.java
+++ Person5.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Hisham
  *  @version 1.1
  */
 public class Person5 {
@@ -30,7 +30,7 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  return input.substring(2, input.length()) + input.substring(2);
+	  return input.substring(2, input.length()) + input.substring(0,2);
 	}
 	
 	/**
