515f647749fadb9ec3713d8ca224f2d667ec1c0c
hishamkodvavi <42811663+hishamkodvavi@users.noreply.github.com>
2018-09-07 10:16:09 -0700

    Update AntLab11.java

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLab11.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLab11.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package edu.gatech.oad.antlab.pkg1;
 
 
@@ -21,28 +20,4 @@ package edu.gatech.oad.antlab.pkg1;
         return "Congrats!";
     }
     
-=======
-package edu.gatech.oad.antlab.pkg1;
-
-
-
-/** 
- * CS2340 Ant Lab
- *
- * AntLab11.java helper class
- * @author Robert
- * @version 1.0
- */
- public class AntLab11 {
-    
-    
-   /**
-    * retrieves a pre-stored string message
-    * @return the string
-    */
-    public String getMessage() {
-        return "Congrats!";
-    }
-    
->>>>>>> master
  } 
