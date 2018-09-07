87e469cdb06503ef963083ce563b9eb863d56758
Chauman4 <adamchau@bellsouth.net>
2018-09-07 11:44:51 -0400

    Changed AntLab Main

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -249,8 +249,8 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p3.toString("sahmed83");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
-                  Person4 p4 = new Person4("P4");
-                  toPrint += p4.toString("gburdell4");
+                  Person4 p4 = new Person4("Adam Chau");
+                  toPrint += p4.toString("achau30");
                   //Person5 replace P4 with your name
                   //and gburdell5 with your gt id
                   Person5 p5 = new Person5("Hisham Kodvavi");
